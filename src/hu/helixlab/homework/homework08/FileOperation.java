d8769a1349d0a41be5236ccd10ebecc7f316086c
Norman Teibert <norman.teibert@gmail.com>
2017-05-06 21:38:00 +0200

    #2

--- src/hu/helixlab/homework/homework08/FileOperation.java
+++ src/hu/helixlab/homework/homework08/FileOperation.java
@@ -26,7 +26,7 @@ public class FileOperation {
                 fileWriter.write("Test");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
